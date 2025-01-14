a472cc173229b4185c4a40be506b6cfbb1e257aa
fred011 <fmmphahle01@gmail.com>
2025-01-14 10:43:49 +0200

    upd1

--- myrouters/class.router.js
+++ myrouters/class.router.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const authMiddleware = require("../Auth/auth");
 
 const {
   createClass,
--- server.js
+++ server.js
@@ -44,7 +44,10 @@ app.options("*", (req, res) => {
     "Access-Control-Allow-Origin",
     "https://erisn-sec-chance-program.vercel.app"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.sendStatus(200);
