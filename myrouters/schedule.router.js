70cfaf1923db62d5c62f97e408749125fd290d53
fred011 <fmmphahle01@gmail.com>
2025-01-20 09:02:20 +0200

    20/01

--- myrouters/schedule.router.js
+++ myrouters/schedule.router.js
@@ -1,15 +1,17 @@
 const express = require("express");
+
 const {
   createSchedule,
   getScheduleWithClass,
   updateScheduleWithId,
   deleteScheduleWithId,
 } = require("../Controllers/schedule.controller");
+
 const router = express.Router();
 
-router.post("/schedule/create", createSchedule);
-router.get("/schedule/class/:classId", getScheduleWithClass);
-router.put("/schedule/:id", updateScheduleWithId);
-router.delete("/schedule/:id", deleteScheduleWithId);
+router.post("/create", createSchedule);
+router.get("/fetch-with-class/:id", getScheduleWithClass);
+router.patch("/update/:id", updateScheduleWithId);
+router.delete("/delete/:id", deleteScheduleWithId);
 
 module.exports = router;
