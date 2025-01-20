c01da004aedaf40d8bb11fc5228ccf7bed85c319
fred011 <fmmphahle01@gmail.com>
2025-01-20 11:48:55 +0200

    20/01

--- Controllers/schedule.controller.js
+++ Controllers/schedule.controller.js
@@ -3,80 +3,85 @@ const Class = require("../Models/class.model");
 const Student = require("../Models/student.model");
 const Exam = require("../Models/examination.model");
 
-// Create a new schedule
-exports.createSchedule = async (req, res) => {
-  try {
-    const { selectedClass, teacher, subject, startTime, endTime } = req.body;
+module.exports = {
+  // Create a new schedule
+  createSchedule: async (req, res) => {
+    try {
+      const { selectedClass, teacher, subject, startTime, endTime } = req.body;
 
-    const newSchedule = new Schedule({
-      classId: selectedClass,
-      teacher,
-      subject,
-      startTime,
-      endTime,
-    });
+      const newSchedule = new Schedule({
+        classId: selectedClass,
+        teacher,
+        subject,
+        startTime,
+        endTime,
+      });
 
-    const savedSchedule = await newSchedule.save();
-    res
-      .status(201)
-      .json({ message: "Schedule created successfully", data: savedSchedule });
-  } catch (error) {
-    res.status(500).json({ message: "Error creating schedule", error });
-  }
-};
-
-// Get schedule by class
-exports.getScheduleWithClass = async (req, res) => {
-  try {
-    const classId = req.params.id;
-    const schedules = await Schedule.find({ class: classId });
-    res.status(200).lson({
-      success: true,
-      message: "Success in fetching all events.",
-      data: schedules,
-    });
-  } catch (error) {
-    console.log("Server Error in retrieving schedules", error);
-    res
-      .status(500)
-      .json({ message: "Server Error in retrieving schedules", error });
-  }
-};
-
-// Update a schedule by ID
-exports.updateScheduleWithId = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updates = req.body;
-
-    const updatedSchedule = await Schedule.findByIdAndUpdate(id, updates, {
-      new: true,
-    });
-    if (!updatedSchedule) {
-      return res.status(404).json({ message: "Schedule not found" });
+      const savedSchedule = await newSchedule.save();
+      res
+        .status(201)
+        .json({
+          message: "Schedule created successfully",
+          data: savedSchedule,
+        });
+    } catch (error) {
+      res.status(500).json({ message: "Error creating schedule", error });
     }
+  },
 
-    res.status(200).json({
-      message: "Schedule updated successfully",
-      data: updatedSchedule,
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Error updating schedule", error });
-  }
-};
-
-// Delete a schedule by ID
-exports.deleteScheduleWithId = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const deletedSchedule = await Schedule.findByIdAndDelete(id);
-    if (!deletedSchedule) {
-      return res.status(404).json({ message: "Schedule not found" });
+  // Get schedule by class
+  getScheduleWithClass: async (req, res) => {
+    try {
+      const classId = req.params.id;
+      const schedules = await Schedule.find({ class: classId });
+      res.status(200).lson({
+        success: true,
+        message: "Success in fetching all events.",
+        data: schedules,
+      });
+    } catch (error) {
+      console.log("Server Error in retrieving schedules", error);
+      res
+        .status(500)
+        .json({ message: "Server Error in retrieving schedules", error });
     }
+  },
 
-    res.status(200).json({ message: "Schedule deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting schedule", error });
-  }
+  // Update a schedule by ID
+  updateScheduleWithId: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updates = req.body;
+
+      const updatedSchedule = await Schedule.findByIdAndUpdate(id, updates, {
+        new: true,
+      });
+      if (!updatedSchedule) {
+        return res.status(404).json({ message: "Schedule not found" });
+      }
+
+      res.status(200).json({
+        message: "Schedule updated successfully",
+        data: updatedSchedule,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Error updating schedule", error });
+    }
+  },
+
+  // Delete a schedule by ID
+  deleteScheduleWithId: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const deletedSchedule = await Schedule.findByIdAndDelete(id);
+      if (!deletedSchedule) {
+        return res.status(404).json({ message: "Schedule not found" });
+      }
+
+      res.status(200).json({ message: "Schedule deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Error deleting schedule", error });
+    }
+  },
 };
