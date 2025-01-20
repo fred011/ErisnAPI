87a1e44fa329c6f2c2586518d64ce03902a5110d
fred011 <fmmphahle01@gmail.com>
2025-01-20 16:09:33 +0200

    20/01

--- myrouters/schedule.router.js
+++ myrouters/schedule.router.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/create", createSchedule);
 router.get("/fetch-with-class/:id", getScheduleWithClass);
-router.patch("/fetch-with-id/:id", fetchScheduleWithId);
+router.patch("/fetch/:id", fetchScheduleWithId);
 router.patch("/update/:id", updateScheduleWithId);
 router.delete("/delete/:id", deleteScheduleWithId);
 
