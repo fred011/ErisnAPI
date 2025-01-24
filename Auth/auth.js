db6ecbcc6423ddf9651aa66e60549590663566bb
fred011 <fmmphahle01@gmail.com>
2025-01-24 19:23:26 +0200

    24/01

--- Auth/auth.js
+++ Auth/auth.js
@@ -3,27 +3,28 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (roles = []) => {
   return (req, res, next) => {
     try {
-      const authHeader = req.header("Authorization");
-      if (!authHeader) {
-        return res.status(401).json({ error: "Authorization header missing." });
-      }
+      const token = req.header("Authorization")?.replace("Bearer ", "");
 
-      const token = authHeader.split(" ")[1];
       if (!token) {
-        return res.status(401).json({ error: "No token provided." });
+        return res
+          .status(401)
+          .json({ success: false, message: "No token provided." });
       }
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded;
 
       if (roles.length > 0 && !roles.includes(req.user.role)) {
-        return res.status(403).json({ error: "Access denied." });
+        return res
+          .status(403)
+          .json({ success: false, message: "Access denied." });
       }
 
       next();
     } catch (error) {
-      console.error("Error decoding token:", error);
-      res.status(401).json({ error: "Invalid token." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid token." });
     }
   };
 };
