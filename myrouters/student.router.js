58338b044c7acfe44747777b0a889a1c3e28d294
fred011 <fmmphahle01@gmail.com>
2025-01-29 10:20:57 +0200

    29/01

--- myrouters/student.router.js
+++ myrouters/student.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const authMiddleware = require("../Auth/auth");
 const router = express.Router();
 const Student = require("../Models/student.model");
 const bcrypt = require("bcryptjs");
