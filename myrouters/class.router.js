abb364db365152005fe5c6fadd815e4408ecd756
fred011 <fmmphahle01@gmail.com>
2025-01-30 16:12:35 +0200

    30/01

--- myrouters/class.router.js
+++ myrouters/class.router.js
@@ -13,8 +13,8 @@ const router = express.Router();
 
 router.post("/create", authMiddleware(["ADMIN"]), createClass); // Only admins can create classes
 router.get("/all", authMiddleware(), getAllClasses); // Protected
-router.get("/single/:id", authMiddleware(), getSingleClass); // Protected
 router.get("/attendee", authMiddleware("TEACHER"), getAttendeeClass); // Protected
+router.get("/single/:id", authMiddleware(), getSingleClass); // Protected
 router.patch("/update/:id", authMiddleware(["ADMIN"]), updateClassWithId); // Only admins can update
 router.delete("/delete/:id", authMiddleware(["ADMIN"]), deleteClassWithId); // Only admins can delete
 
